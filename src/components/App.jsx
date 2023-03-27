e0ebec02678a991ba2a9718919c34443eebba7dd
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-03-27 16:41:26 +0200

    total Todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,57 @@
+import { Component } from 'react';
+
 import { Counter } from './Counter/Counter';
 import { Dropdown } from './Dropdown/Dropdown';
 import { ColorPicker } from './ColorPicker/ColorPicker';
+import { TodoList } from './TodoList';
 
-export const App = () => {
-  const colorPickerOptions = [
-    { label: 'red', color: '#F44336' },
-    { label: 'green', color: '#4CAF50' },
-    { label: 'blue', color: '#2196F3' },
-    { label: 'grey', color: '#607D8B' },
-    { label: 'pink', color: '#3F5185' },
-  ];
+export class App extends Component {
+  state = {
+    todos: [
+      { id: 'id-1', text: 'Todo 1', completed: false },
+      { id: 'id-2', text: 'Todo 2', completed: false },
+      { id: 'id-3', text: 'Todo 3', cpmpleted: true },
+      { id: 'id-4', text: 'Todo 4', completed: false },
+      { id: 'id-5', text: 'Todo 5', completed: true },
+    ],
+  };
 
-  // return <Counter initialValue={22} />;
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
 
-  return (
-    <>
-      {/* <Counter initialValue={22} /> */}
-      {/* <Dropdown /> */}
+  render() {
+    const { todos } = this.state;
+    const totalTodo = todos.length;
+    const doneTodo = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
 
-      <ColorPicker title="Color Picker" colors={colorPickerOptions} />
-    </>
-  );
-};
+    // const colorPickerOptions = [
+    //   { label: 'red', color: '#F44336' },
+    //   { label: 'green', color: '#4CAF50' },
+    //   { label: 'blue', color: '#2196F3' },
+    //   { label: 'grey', color: '#607D8B' },
+    //   { label: 'pink', color: '#3F5185' },
+    // ];
+
+    // return <Counter initialValue={22} />;
+
+    return (
+      <>
+        {/* <Counter initialValue={22} /> */}
+        {/* <Dropdown /> */}
+
+        {/* <ColorPicker title="Color Picker" colors={colorPickerOptions} /> */}
+        <div>
+          <span>__Total: {totalTodo}</span>
+          <span>__Done: {doneTodo}</span>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
